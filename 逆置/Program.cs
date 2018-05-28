cc96ca2c8a4553e75b69da04361359eecfa308ad
一心 <ulquiola@users.noreply.github.com>
2018-05-28 11:05:03 +0800

    seven

---
+++ 逆置/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 逆置
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string2;
+            string string1 = Console.ReadLine();
+            for(int i=string1.Length-1;i>=0;i--)
+            {
+                string2 = string1;
+                Console.Write(string2[i]);
+            }
+        }
+    }
+}
